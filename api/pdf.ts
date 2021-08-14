5b8c1a15e6fed1c41f2dc11b9eadcffaa36bb88d
ksmarty <kyleschwartz2000@gmail.com>
2021-08-14 12:47:21 -0400

    Serverless 2!

--- api/pdf.ts
+++ api/pdf.ts
@@ -1,6 +1,8 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function (req: VercelRequest, res: VercelResponse) {
-	const { name = "World" } = req.query;
-	res.send(`Hello ${name}!`);
+	// const { name = "World" } = req.query;
+	res.send(req.body);
+
+	// https://cors-anywhere.herokuapp.com/https://cdn.discordapp.com/attachments/799412047501590528/876144049067806790/Getting_Started.pdf
 }
